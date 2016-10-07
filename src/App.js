a970dd40caeed8ef1b7185daebe36bc855fad478
Anders Sjöberg <anders.sjoberg@visma.com>
2016-10-08 01:13:56 +0200

    Mer labb

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 import Input from './input'
 import Signature from './images/Signature.svg'
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ''
+        }
+    }
+    clickSend = (e) => {
+        this.setState({
+            value: e
+        })
+    }
 
-  clickSend(e) {
-    console.log(e)
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Wilkommen aus der React schmutzfabrik</h2>
-          <svg src={Signature} />
-        </div>
-        <Input clickSend={this.clickSend} />
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Wilkommen aus der React schmutzfabrik</h2>
+            </div>
+            <Input clickSend={this.clickSend} />
+            <span>{this.state.value}</span>
+            <img role="presentation" className="signature" src={Signature} />
+          </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/input.js
+++ src/input.js
@@ -1,21 +1,34 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import './Input.css'
 
 class Input extends Component {
-    propTypes = {
+    constructor(props) {
+        super(props)
+        this.state = {
+            nameTxt: ''
+        }
+    }
+
+    static propTypes = {
         clickSend: React.PropTypes.func.isRequired
     }
+
+    handleChange = (event) => {
+        this.props.clickSend(event.target.value)
+        this.setState(
+            {nameTxt: event.target.value}
+        )
+    }
+
     render() {
         return (
             <div className="input-container">
                 <label htmlFor="name">Name: </label>
-                <input id="name" type="text" />
-                <button className="sendbutton" onClick={this.props.clickSend.bind(this) } >Send</button>
+                <input id="name" type="text" value={this.state.nameTxt} onChange={this.handleChange}/>
+                <button className="sendbutton" onClick={this.props.clickSend.bind(this, this.state.nameTxt) } >Send</button>
             </div>
-        );
+        )
     }
 }
 
-Input.
-
-export default Input;
+export default Input
